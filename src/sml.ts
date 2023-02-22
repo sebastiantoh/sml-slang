3ece4d077061e5544fa58091d554b12c84dbcec1
Sebastian Toh <sebas.tsj.98@gmail.com>
2023-02-22 21:46:37 +0800

    shift builtin bin operators to sml.ts

--- src/sml.ts
+++ src/sml.ts
@@ -49,3 +49,157 @@ export const valueToString = (sml_val: Value) => {
       return sml_val.js_val.toString()
   }
 }
+
+// Full list of builtins along with precedence can be found on
+// Page 98 of https://smlfamily.github.io/sml90-defn.pdf
+export const builtinBinOperators = {
+  '/': (a: Value, b: Value) => {
+    if (a.type === 'real' && b.type === 'real') {
+      return {
+        type: 'real',
+        // TODO: handle div by 0?
+        js_val: a.js_val / b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  div: (a: Value, b: Value) => {
+    if (a.type === 'int' && b.type === 'int') {
+      return {
+        type: 'int',
+        // TODO: handle div by 0?
+        js_val: Math.floor(a.js_val / b.js_val)
+      }
+    }
+    throw new Error('invalid types')
+  },
+  mod: (a: Value, b: Value) => {
+    if (a.type === 'int' && b.type === 'int') {
+      return {
+        type: 'int',
+        js_val: ((a.js_val % b.js_val) + b.js_val) % b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '*': (a: Value, b: Value) => {
+    if ((a.type === 'int' && b.type === 'int') || (a.type === 'real' && b.type === 'real')) {
+      return {
+        type: a.type,
+        js_val: a.js_val * b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '+': (a: Value, b: Value) => {
+    if ((a.type === 'int' && b.type === 'int') || (a.type === 'real' && b.type === 'real')) {
+      return {
+        type: a.type,
+        js_val: a.js_val + b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '-': (a: Value, b: Value) => {
+    if ((a.type === 'int' && b.type === 'int') || (a.type === 'real' && b.type === 'real')) {
+      return {
+        type: a.type,
+        js_val: a.js_val - b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '^': (a: Value, b: Value) => {
+    if (a.type === 'string' && b.type === 'string') {
+      return {
+        type: 'string',
+        js_val: a.js_val.concat(b.js_val)
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '=': (a: Value, b: Value) => {
+    if (
+      (a.type === 'int' && b.type === 'int') ||
+      (a.type === 'real' && b.type === 'real') ||
+      (a.type === 'string' && b.type === 'string') ||
+      (a.type === 'char' && b.type === 'char')
+    ) {
+      return {
+        type: 'bool',
+        js_val: a.js_val === b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '<>': (a: Value, b: Value) => {
+    if (
+      (a.type === 'int' && b.type === 'int') ||
+      (a.type === 'real' && b.type === 'real') ||
+      (a.type === 'string' && b.type === 'string') ||
+      (a.type === 'char' && b.type === 'char')
+    ) {
+      return {
+        type: 'bool',
+        js_val: a.js_val !== b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '<': (a: Value, b: Value) => {
+    if (
+      (a.type === 'int' && b.type === 'int') ||
+      (a.type === 'real' && b.type === 'real') ||
+      (a.type === 'string' && b.type === 'string') ||
+      (a.type === 'char' && b.type === 'char')
+    ) {
+      return {
+        type: 'bool',
+        js_val: a.js_val < b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '>': (a: Value, b: Value) => {
+    if (
+      (a.type === 'int' && b.type === 'int') ||
+      (a.type === 'real' && b.type === 'real') ||
+      (a.type === 'string' && b.type === 'string') ||
+      (a.type === 'char' && b.type === 'char')
+    ) {
+      return {
+        type: 'bool',
+        js_val: a.js_val > b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '<=': (a: Value, b: Value) => {
+    if (
+      (a.type === 'int' && b.type === 'int') ||
+      (a.type === 'real' && b.type === 'real') ||
+      (a.type === 'string' && b.type === 'string') ||
+      (a.type === 'char' && b.type === 'char')
+    ) {
+      return {
+        type: 'bool',
+        js_val: a.js_val <= b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  },
+  '>=': (a: Value, b: Value) => {
+    if (
+      (a.type === 'int' && b.type === 'int') ||
+      (a.type === 'real' && b.type === 'real') ||
+      (a.type === 'string' && b.type === 'string') ||
+      (a.type === 'char' && b.type === 'char')
+    ) {
+      return {
+        type: 'bool',
+        js_val: a.js_val >= b.js_val
+      }
+    }
+    throw new Error('invalid types')
+  }
+}
