1b218f6ac726a08b59cdff76b756a3635c3b32da
Rohit <rajeshbhatrohit@gmail.com>
2023-02-25 12:15:19 +0800

    add estree and sourceerror types

---
+++ src/errors/types.ts
@@ -0,0 +1,21 @@
+import * as es from 'estree'
+
+export enum ErrorType {
+  SYNTAX = 'Syntax',
+  TYPE = 'Type',
+  RUNTIME = 'Runtime'
+}
+
+export enum ErrorSeverity {
+  WARNING = 'Warning',
+  ERROR = 'Error'
+}
+
+// any and all errors ultimately implement this interface. as such, changes to this will affect every type of error.
+export interface SourceError {
+  type: ErrorType
+  severity: ErrorSeverity
+  location: es.SourceLocation
+  explain(): string
+  elaborate(): string
+}
