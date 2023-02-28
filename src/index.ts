fc249af0bc5de34b5a357069140948d4815e4e6d
Rohit <rajeshbhatrohit@gmail.com>
2023-02-28 19:26:21 +0800

    add parseError method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import { evaluate } from './interpreter/interpreter'
 import { parseProg } from './parser/parser'
-import * as Sml from './sml'
 import { Context } from './types'
+import { SourceError, Value } from './types'
 
-export function run(code: string, context: Context): Sml.Value {
+export function run(code: string, context: Context): Value {
   const program = parseProg(code)
   // TODO: type checking, error handling etc.
   return evaluate(program)
+}
+
+export function parseError(errors: SourceError[]): string {
+  const errorMessagesArr = errors.map(error => {
+    const line = error.location ? error.location.start.line : '<unknown>'
+    const explanation = error.explain()
+    return line < 1 ? explanation : `Line ${line}: ${explanation}`
+  })
+  return errorMessagesArr.join('\n')
 }
