1c60d2d6291e86f2ef191b486b96ed9db502fdf9
Rohit <rajeshbhatrohit@gmail.com>
2023-03-13 17:08:49 +0800

    add functyp

--- src/typechecker/types.ts
+++ src/typechecker/types.ts
@@ -1,3 +1,9 @@
-export type Type = PrimitiveType
+export type Type = PrimitiveType | FunctionType
 
 export type PrimitiveType = 'int' | 'real' | 'string' | 'char' | 'bool' | 'unit'
+
+// to simplify typechecking, we curry multi param funcs
+export interface FunctionType {
+  parameterType: Type
+  returnType: Type
+}
