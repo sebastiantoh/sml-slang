371c03c9f96666afed65c3e3d6b6e6112248ac1a
sebastiantoh <sebas.tsj.98@gmail.com>
2023-03-21 13:29:26 +0000

    Deploying to dist from @ sebastiantoh/sml-slang@ad520e9fb3d6b5b3d17e1859ce510639ef0f2159 🚀

--- __tests__/pattern.js
+++ __tests__/pattern.js
@@ -12,5 +12,25 @@ in
   begins_with_one [1,2,3]
 end
 `)).toBe(`true`));
+    test('multiple infix construction pattern matching', () => expect((0, utils_1.parseAndEvaluateExp)(`
+let
+  fun begins_with_one_and_two lst =
+    case lst of
+     1::2::_ => true
+    | _ => false
+in
+  begins_with_one_and_two [1,2,3]
+end
+`)).toBe(`true`));
+    test('infix construction pattern matching with variable', () => expect((0, utils_1.parseAndEvaluateExp)(`
+let
+  fun hd lst =
+    case lst of
+      x::xs => x
+    | _ => 0
+in
+  hd [123, 456]
+end
+`)).toBe(`123`));
 });
 //# sourceMappingURL=pattern.js.map
