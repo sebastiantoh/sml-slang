58a726b42c4db24892c52c75e6f452ee9f6c7022
Rohit <rajeshbhatrohit@gmail.com>
2023-03-23 14:30:11 +0800

    add types needed for typechecker

--- src/typechecker/types.ts
+++ src/typechecker/types.ts
@@ -1,9 +1,24 @@
-export type Type = PrimitiveType | MatchType
+export type Type = PrimitiveType | FunctionType | ListType | TypeVariable
 
 export type PrimitiveType = 'int' | 'real' | 'string' | 'char' | 'bool' | 'unit'
 
 // to simplify typechecking, we curry multi param funcs
-export interface MatchType {
+export interface FunctionType {
   parameterType: Type
   returnType: Type
+}
+
+export interface ListType {
+  elementType: Type
+}
+
+// 0: 'a, 1: 'b, ....
+// can format nicely when printing
+export interface TypeVariable {
+  id: number
+}
+
+export interface TypeScheme {
+  type: Type
+  typeVariables: TypeVariable[]
 }
