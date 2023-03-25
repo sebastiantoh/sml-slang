847c431a7e2a2265927fcc20eac1e347e7cfed0a
Rohit <rajeshbhatrohit@gmail.com>
2023-03-26 00:05:10 +0800

    add list type inference

--- src/typechecker/index.ts
+++ src/typechecker/index.ts
@@ -17,18 +17,31 @@ export function hindleyMilner(env: TypeEnvironment, node: Node): [Type, TypeCons
     case 'StringConstant':
     case 'CharConstant':
     case 'BoolConstant':
-    case 'UnitConstant':
+    case 'UnitConstant': {
       return [node.type, []]
+    }
     // Variable
-    case 'Variable':
+    case 'Variable': {
       const ts = getTypeSchemeFromEnv(env, node.id)
       return [instantiate(ts), []]
+    }
     // Application
-    case 'Application':
+    case 'Application': {
       const t = freshTypeVariable()
       const [t1, C1] = hindleyMilner(env, node.fn)
       const [t2, C2] = hindleyMilner(env, node.arg)
       return [t, [...C1, ...C2, { type1: t1, type2: { parameterType: t2, returnType: t } }]]
+    }
+    // List
+    case 'ListLiteral': {
+      const t = freshTypeVariable()
+      let C: TypeConstraint[] = []
+      for (const el of node.elements) {
+        const [tmp_ty, tmp_C] = hindleyMilner(env, el)
+        C = [...C, ...tmp_C, { type1: t, type2: tmp_ty }]
+      }
+      return [t, C]
+    }
   }
 
   return [UNIT_TY, []]
