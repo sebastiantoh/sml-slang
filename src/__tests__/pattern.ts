4f46ec44e5b1562e7b96deff3e761cf2af7e49e4
Radhya <mradhyaf@gmail.com>
2023-03-27 10:26:17 +0800

    Uncomment some tests

--- src/__tests__/pattern.ts
+++ src/__tests__/pattern.ts
@@ -1,48 +1,48 @@
 import { parseAndEvaluateExp } from './utils'
 
-// describe('InfixConstruction', () => {
-//   test('infix construction pattern matching', () =>
-//     expect(
-//       parseAndEvaluateExp(`
-// let
-//   fun begins_with_one lst =
-//     case lst of
-//      1::_ => true
-//     | _ => false
-// in
-//   begins_with_one [1,2,3]
-// end
-// `)
-//     ).toBe(`true`))
+describe('InfixConstruction', () => {
+  test('infix construction pattern matching', () =>
+    expect(
+      parseAndEvaluateExp(`
+let
+  fun begins_with_one lst =
+    case lst of
+     1::_ => true
+    | _ => false
+in
+  begins_with_one [1,2,3]
+end
+`)
+    ).toBe(`true`))
 
-//   test('multiple infix construction pattern matching', () =>
-//     expect(
-//       parseAndEvaluateExp(`
-// let
-//   fun begins_with_one_and_two lst =
-//     case lst of
-//      1::2::_ => true
-//     | _ => false
-// in
-//   begins_with_one_and_two [1,2,3]
-// end
-// `)
-//     ).toBe(`true`))
+  test('multiple infix construction pattern matching', () =>
+    expect(
+      parseAndEvaluateExp(`
+let
+  fun begins_with_one_and_two lst =
+    case lst of
+     1::2::_ => true
+    | _ => false
+in
+  begins_with_one_and_two [1,2,3]
+end
+`)
+    ).toBe(`true`))
 
-//   test('infix construction pattern matching with variable', () =>
-//     expect(
-//       parseAndEvaluateExp(`
-// let
-//   fun hd lst =
-//     case lst of
-//       x::xs => x
-//     | _ => 0
-// in
-//   hd [123, 456]
-// end
-// `)
-//     ).toBe(`123`))
-// })
+  test('infix construction pattern matching with variable', () =>
+    expect(
+      parseAndEvaluateExp(`
+let
+  fun hd lst =
+    case lst of
+      x::xs => x
+    | _ => 0
+in
+  hd [123, 456]
+end
+`)
+    ).toBe(`123`))
+})
 
 describe('ListPattern', () => {
   test('list pattern matching with variables', () =>
