a612a00b1d4c665cbb9f859a76430ba8e84c070f
sebastiantoh <sebas.tsj.98@gmail.com>
2023-03-29 11:44:55 +0000

    Deploying to dist from @ sebastiantoh/sml-slang@bb1f061bf76dc08558509349e4f1a827438fdffc 🚀

--- typechecker/__tests__/expressions.js
+++ typechecker/__tests__/expressions.js
@@ -104,8 +104,7 @@ end
     test('function type inferred from :: operator in pattern', () => expect((0, utils_1.parseAndTypeCheckExp)(`fn fst::snd::tl => (fst+snd)::tl`)).toBe(`int list -> int list`));
     test('function type inferred from :: operator in pattern with type variable in param and return type', () => expect((0, utils_1.parseAndTypeCheckExp)(`fn fst::snd::tl => fst::tl`)).toBe(`'a list -> 'a list`));
     test('function type inferred from @ operator', () => expect((0, utils_1.parseAndTypeCheckExp)(`fn lists_of_list => lists_of_list @ [[1,2,3],[4,5,6]]`)).toBe(`int list list -> int list list`));
-    // TODO fix this case
-    test.skip('function with acc', () => expect((0, utils_1.parseAndTypeCheckExp)(`
+    test('type inference of recursive functions', () => expect((0, utils_1.parseAndTypeCheckExp)(`
 let
   fun loop acc lst =
     case lst of [] => acc | x::xs => loop (acc + 1) (xs)
