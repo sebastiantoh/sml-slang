780a435e2dfb9897f0c38d990c882797b671b3e1
sebastiantoh <sebas.tsj.98@gmail.com>
2023-04-02 14:02:17 +0000

    Deploying to dist from @ sebastiantoh/sml-slang@818bf1f7110323e76eebcb9392df333ac38fee2d 🚀

--- typechecker/__tests__/utils.js
+++ typechecker/__tests__/utils.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseAndTypeCheckExp = void 0;
+const __1 = require("../..");
 const parser_1 = require("../../parser/parser");
-const __1 = require("..");
+const __2 = require("..");
 const environment_1 = require("../environment");
 const utils_1 = require("../utils");
-const __2 = require("../..");
 const parseAndTypeCheckExp = (exp) => {
     const node = (0, parser_1.parseExp)(exp);
-    const [type, typeConstraints] = (0, __1.hindleyMilner)(__2.INIT_ENV, node);
+    const [type, typeConstraints] = (0, __2.hindleyMilner)(__1.INIT_ENV, node);
     const solvedType = (0, environment_1.unifyAndSubstitute)(type, typeConstraints);
     return (0, utils_1.stringifyType)(solvedType);
 };
